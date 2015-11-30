0a2ecaf6f7e9e518cd47e620f3734a49984fe6cb
Isaac <isaacgoss14750@gmail.com>
2015-11-30 11:46:39 -0500

    All done

--- Lab13/BadClass.java
+++ Lab13/BadClass.java
@@ -12,5 +12,27 @@ public class BadClass {
         reader.close();
     }
     
+    public void makeArray() throws NegativeArraySizeException {
+        int[] uhoh = new int[-1];
+    }
+    
+    public void accessArray() throws ArrayIndexOutOfBoundsException {
+        int[] ohno = new int[3];
+        int baddy = ohno[4];
+    }
+    
+    public void addToArray() throws ArrayStoreException {
+        Object x[] = new String[3];
+        x[0] = new Integer(0);
+    }
+    
+    public void badMath() throws ArithmeticException {
+        int nope = 1/0;
+    }
+    
+    public void strConverter() throws NumberFormatException {
+        String greg = "Hello, world!";
+        int brady = Integer.parseInt(greg);
+    }
     
 }
--- Lab13/Lab13.java
+++ Lab13/Lab13.java
@@ -25,11 +25,35 @@ public class Lab13 {
             exPrinter(fnf.toString());
         }
         
-        // try {
-            
-        // }
+        try {
+            bc.makeArray();
+        } catch (NegativeArraySizeException nas) {
+            exPrinter(nas.toString());
+        }
         
+        try {
+            bc.accessArray();
+        } catch (ArrayIndexOutOfBoundsException aiob) {
+            exPrinter(aiob.toString());
+        }
         
+        try {
+            bc.addToArray();
+        } catch (ArrayStoreException cc) {
+            exPrinter( cc.toString());
+        }
+        
+        try {
+            bc.badMath();
+        } catch (ArithmeticException ar) {
+            exPrinter(ar.toString());
+        }
+        
+        try {
+            bc.strConverter();
+        } catch (NumberFormatException nf ) {
+            exPrinter(nf.toString());
+        }
     }
     
     private static void exPrinter(String ex) {
